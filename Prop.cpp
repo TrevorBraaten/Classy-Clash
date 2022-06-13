4bb5552e7d947f9c3bf4a487a2c80191130cb602
Trevor Braaten <80365363+TrevorBraaten@users.noreply.github.com>
2022-06-13 00:38:17 -0700

    Initial Commit

--- Prop.cpp
+++ Prop.cpp
@@ -1,4 +1,5 @@
 #include "Prop.h"
+#include "raymath.h"
 
 
 Prop::Prop(Vector2 pos, Texture2D tex):
@@ -10,7 +11,8 @@ texture(tex)
 
 }
 
-Vector2 Prop::Render(Vector2 np)
+void Prop::Render(Vector2 KnightPos)
 {
-
+    Vector2 screenPos{ Vector2Subtract(worldPos, knightPos) };
+    DrawTextureEx(texture, screenPos, 0.f, scale, WHITE);
 }
--- Prop.h
+++ Prop.h
@@ -6,11 +6,11 @@ class Prop
 {
     public:
     Prop(Vector2 pos, Texture2D tex);
-    Vector2 Render(Vector2 np);
+    void Render(Vector2 knightPos);
 
     private:
     Texture2D texture{};
     Vector2 worldPos{};
-    Vector2 np{};
+    Vector2 knightPos{};
     float scale{4.f};
 };
