355de7b876cc9fe4486cd6041d84f538e985b40f
Trevor Braaten <80365363+TrevorBraaten@users.noreply.github.com>
2022-06-14 01:19:04 -0700

    Initial Commiat

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,12 +2,15 @@
 #include "raymath.h"
 
 
-Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) : 
-worldPos(pos), 
-texture(idle_texture),
-idle(idle_texture),
-run(run_texture)
+Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) 
+
 {
+
+    worldPos = pos;
+    texture = idle_texture;
+    idle = idle_texture;
+    run = run_texture;
+
     width = texture.width / maxFrames;
     height = texture.height;
 }
@@ -33,17 +36,3 @@ void Enemy::tick(float deltaTime)
    
 }
 
-void Enemy::undoMovement()
-{
-    worldPos = worldPosLastFrame;
-}
-
-Rectangle Enemy::getCollisionRec()
-{
-    return Rectangle{
-        screenPos.x,
-        screenPos.y,
-        width * scale,
-        height * scale
-    };
-}
